0913b8387e37c014637266c3a1dd7aa4356ee6fc
snehasivakumar23 <sneha.sivakumar@kellogg.northwestern.edu>
2020-02-23 23:18:47 +0000

    push

--- app.rb
+++ app.rb
@@ -19,9 +19,23 @@ get "/news" do
     @geocoder_results = Geocoder.search(@location)
     @lat_long = @geocoder_results.first.coordinates # => [lat, long] array
     @forecast = ForecastIO.forecast(@lat_long[0],@lat_long[1]).to_hash
+
+   #get future forecast
+    day_hightemp = []
+    day_condition = []
+    for day in @forecast["daily"]["data"]
+        day_hightemp << "#{day["temperatureHigh"]}"
+        day_condition << "#{day["summary"]}"
+    #"A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}."
+    end
+    @listtemp = day_hightemp
+    @listcondition = day_condition
+
+
+
     #"#{@forecast}"
     @url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=be23bdea6d684a30a91332a123eff0d2"
-    @news = HTTParty.get(url).parsed_response.to_hash
+    @news = HTTParty.get(@url).parsed_response.to_hash
  
     view "news"
 end
